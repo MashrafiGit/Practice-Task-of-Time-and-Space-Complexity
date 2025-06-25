856b05bb8fcb21b21de16af3d4e4f713c9fd6b81
Masrafi404 <mdmasrafi902@gmail.com>
2025-06-25 19:50:26 +0600

    find problem seven time complexity

---
+++ seven.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n = 100;
+    int a[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        sort(a, a + n);
+    }
+
+    return 0;
+}
+
+// time complexity: O(n * NlogN)
